fac2e673624a2eed1fce6646790924201c0a1d36
AmberAAA <qe-d@163.com>
2019-01-15 12:43:22 +0800

    .

--- modules/util.py
+++ modules/util.py
@@ -2,9 +2,20 @@ import urllib.parse
 import urllib.request
 import json
 
+max_try_num = 4
+
 def post(url, data, method="POST"):
     post_data = urllib.parse.urlencode(data).encode('utf-8')
-    request = urllib.request.Request(url, data=post_data, method=method)
-    response = urllib.request.urlopen(request)
+    for tries in range(max_try_num):
+        try:
+            request = urllib.request.Request(url, data=post_data, method=method)
+            response = urllib.request.urlopen(request)
+            break
+        except:
+            if tries < (max_try_num - 1) :
+                continue
+            else:
+                print("post failed!")
+                break
     return response.read().decode('utf-8')
 
